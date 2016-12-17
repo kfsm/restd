5ce4fb312b7d97dc409d3ea681fcc81e6b5a7662
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2016-12-17 18:39:47 +0200

    enable path option for webapp endpoint

--- src/restd_api_webapp.erl
+++ src/restd_api_webapp.erl
@@ -21,8 +21,9 @@
 
 -export([
    allowed_methods/1,
-	content_provided/1, 
-	exists/2,
+   content_provided/1,
+   cors/1,
+   exists/2,
    'GET'/3
 ]).
 
@@ -36,18 +37,26 @@ content_provided(_Req) ->
 
 %%
 %%
+cors(_Req) ->
+   [
+      {'Access-Control-Allow-Origin', <<"*">>}
+     ,{'Access-Control-Allow-Methods', <<"GET">>}
+     ,{'Access-Control-Max-Age', 86400}
+   ].
+
+%%
+%%
 exists(_, {Url, _Heads, Env}) ->
-	filelib:is_file(filename(Url, Env)).
+   filelib:is_file(filename(Url, Env)).
 
 %%
 %%
 'GET'(_Type, _Msg, {Url, _Heads, Env}) ->
-	Filename   = filename(Url, Env),
-	{ok, File} = file:read_file(Filename),
-	{ok, 
+   Filename   = filename(Url, Env),
+   {ok, File} = file:read_file(Filename),
+   {ok, 
       [
          {'Content-Type', mime(filename:extension(Filename))}
-        ,{'Access-Control-Allow-Origin', <<"*">>}
       ], 
       File
    }.
@@ -55,51 +64,68 @@ exists(_, {Url, _Heads, Env}) ->
 %%
 %%
 filename(Url, Env) ->
-	case opts:val(<<"file">>, undefined, Env) of
-		undefined ->	
-			case uri:segments(Url) of
-				undefined -> 
-				   filename:join([htdoc(Env), "index.html"]);
-				[]        ->
-				   filename:join([htdoc(Env), "index.html"]);
-				List      ->
-					filename:join([htdoc(Env)] ++ [scalar:c(X) || X <- List, X =/= <<".">>, X =/= <<"..">>, X =/= <<$~>>])
-			end;
-		File      ->
-			filename:join([htdoc(Env), scalar:c(File)])
-	end.
+   case opts:val(<<"file">>, undefined, Env) of
+      undefined ->
+         filename:join([htdoc(Env), path(Env), file(Url)]);
+      File ->
+         filename:join([htdoc(Env), path(Env), hd(segments([scalar:s(File)]))])
+   end.
 
 %%
 %% 
 htdoc(Env) ->
-	case opts:val(htdoc, Env) of
-		X when is_atom(X) -> 
-			case code:priv_dir(X) of
-				{error,bad_name} -> 
-					filename:join([priv, htdoc]);
-				Root -> 
-					filename:join([Root, htdoc])
-			end;
-		X when is_list(X) ->
-			X 
-	end.
+   case opts:val(htdoc, Env) of
+      X when is_atom(X) -> 
+         case code:priv_dir(X) of
+            {error,bad_name} -> 
+               filename:join([priv, htdoc]);
+            Root -> 
+               filename:join([Root, htdoc])
+         end;
+      X when is_list(X) ->
+         X 
+   end.
 
 %%
 %%
-mime([])       -> {text, html};
-mime(".html")  -> {text, html};
-mime(".txt")   -> {text, plain};
-mime(".css")   -> {text, css};
-mime(".js")    -> {text, javascript};
-mime(".xml")   -> {text, xml};
+path(Env) ->
+   case opts:val(path, undefined, Env) of
+      undefined ->
+         <<>>;
+      Path ->
+         Path
+   end.
 
-mime(".json")  -> {application, json};
-mime(".png")   -> {image, png};
-mime(".jpg")   -> {image, jpeg};
-mime(".jpeg")  -> {image, jpeg};
-mime(".svg")   -> {image, 'svg+xml'};
+%%
+%%
+segments(Segments) ->
+   [scalar:s(X) || X <- Segments, X =/= <<".">>, X =/= <<"..">>, X =/= <<$~>>].
+
+%%
+%%
+file(Url) ->
+   case uri:segments(Url) of
+      undefined -> <<"index.html">>;
+      []        -> <<"index.html">>;
+      Segments  -> segments(Segments)
+   end.
 
 
-mime(".pdf")   -> {application, pdf};
+%%
+%%
+mime(<<>>)         -> {text, html};
+mime(<<".html">>)  -> {text, html};
+mime(<<".txt">>)   -> {text, plain};
+mime(<<".css">>)   -> {text, css};
+mime(<<".js">>)    -> {text, javascript};
+mime(<<".xml">>)   -> {text, xml};
 
-mime(_)        -> {application, 'octet-stream'}.
+mime(<<".json">>)  -> {application, json};
+mime(<<".png">>)   -> {image, png};
+mime(<<".jpg">>)   -> {image, jpeg};
+mime(<<".jpeg">>)  -> {image, jpeg};
+mime(<<".svg">>)   -> {image, 'svg+xml'};
+
+mime(<<".pdf">>)   -> {application, pdf};
+
+mime(_)            -> {application, 'octet-stream'}.
